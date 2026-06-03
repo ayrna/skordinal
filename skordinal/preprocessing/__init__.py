f6ce634701fca0570ba34ad8525a34f8ffca3879
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-06-03 20:43:27 +0200

    MNT: Remove deprecated preprocessing scaler wrappers

--- skordinal/preprocessing/__init__.py
+++ skordinal/preprocessing/__init__.py
@@ -5,13 +5,9 @@ from ._encodings import (
     build_coding_matrix,
     ordinal_to_binary_cumulative,
 )
-from ._scalers import apply_scaling, minmax_scale, standardize
 
 __all__ = [
-    "apply_scaling",
     "binary_cumulative_to_ordinal",
     "build_coding_matrix",
-    "minmax_scale",
     "ordinal_to_binary_cumulative",
-    "standardize",
 ]
