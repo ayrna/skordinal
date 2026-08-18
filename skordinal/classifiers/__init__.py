6f96a11bec2817854b703ebf071c06cfdbf11058
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-08-18 17:28:03 +0200

    ENH: Add LogisticIT and LogisticAT (#110)
    * ENH: Add LogisticIT immediate-threshold ordinal classifier
    
    * ENH: Add LogisticAT all-threshold ordinal classifier

--- skordinal/classifiers/__init__.py
+++ skordinal/classifiers/__init__.py
@@ -3,6 +3,8 @@
 from ._cost_sensitive_wrapper import CostSensitiveWrapper
 from ._elmop import ELMOP
 from ._kdlor import KDLOR
+from ._logistic_at import LogisticAT
+from ._logistic_it import LogisticIT
 from ._nnop import NNOP
 from ._nnpom import NNPOM
 from ._orboost import ORBoost
@@ -16,6 +18,8 @@ __all__ = [
     "CostSensitiveWrapper",
     "ELMOP",
     "KDLOR",
+    "LogisticAT",
+    "LogisticIT",
     "NNOP",
     "NNPOM",
     "ORBoost",
