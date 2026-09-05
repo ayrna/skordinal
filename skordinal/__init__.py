68e49f9d0a4ef03244953923308888718fac8605
Ángel Sevilla Molina <angelsevillamol@gmail.com>
2026-09-05 19:12:22 +0200

    REL: Release 0.2.0 (#172)

--- skordinal/__init__.py
+++ skordinal/__init__.py
@@ -3,7 +3,7 @@
 import logging
 
 __all__: list[str] = []
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # Library convention: emit records, never configure handlers for the app
 _logger = logging.getLogger("skordinal")
